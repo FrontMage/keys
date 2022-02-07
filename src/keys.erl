dc8a91ae994c7f83d176bacddd956da60fb4f58f
FrontMage <xbgxwh@outlook.com>
2022-02-08 01:39:17 +0800

    init

--- src/keys.erl
+++ src/keys.erl
@@ -10,7 +10,7 @@
 %% escript Entry point
 main(_Args) ->
     _Key = ecc_compact:generate_key(),
-    SwarmKey = filename:join(["/mnt/1TB/helium_ansible/miner_keys/161.35.4.5", "miner", "swarm_key"]),
+    SwarmKey = filename:join(["./", "miner", "swarm_key"]),
     ok = filelib:ensure_dir(SwarmKey),
     case libp2p_crypto:load_keys(SwarmKey) of
         {ok, #{secret := _PrivKey0, public := PubKey}} ->
